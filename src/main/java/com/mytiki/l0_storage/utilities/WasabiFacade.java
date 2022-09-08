d69768fb129c7f4ae4cb979a7aab7f6a73fdff37
Mike Audi <mike@mytiki.com>
2022-09-08 01:09:02 -0500

    fixed typo in aws credential builder

--- src/main/java/com/mytiki/l0_storage/utilities/WasabiFacade.java
+++ src/main/java/com/mytiki/l0_storage/utilities/WasabiFacade.java
@@ -58,6 +58,6 @@ public class WasabiFacade {
     }
 
     public String buildCredential(String date){
-        return wasabiKey + "/" + date + "/" + wasabiRegion + "/s3/awsaws4_request";
+        return wasabiKey + "/" + date + "/" + wasabiRegion + "/s3/aws4_request";
     }
 }
